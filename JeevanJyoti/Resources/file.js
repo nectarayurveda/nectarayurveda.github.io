4038055ca284d5a485cafe9382478d320f83cd81
CharakSamhita <CharakSamhita@users.noreply.github.com>
2020-07-18 00:35:02 +0530

    18

--- JeevanJyoti/Resources/file.js
+++ JeevanJyoti/Resources/file.js
@@ -125,4 +125,58 @@ function dAddD() {
     document.getElementById("dDurD").appendChild(document.createElement("hr"))
      
 }
+/*medicine prescription*/
+function medAdd()
+{
+    var node = document.createElement("p") ;
+                                      
+    var textnode = document.createTextNode(document.getElementById("meds").value);
+    
+    node.appendChild(textnode);
+    document.getElementById("medicineCombination").appendChild(node);
+    
+}
+
+/*##########//patient details//#################*/
+
+
+function patientInfo(id,name,age,gender,height,weight,MoNo,email,symptom,disease)
+{
+    
+    //making object
+    this.Id=id;
+    this.Name=name;
+    this.Age=age;
+    this.Gender=gender;
+    this.Height=height;
+    this.Weight=weight;
+    this.MobileNo=MoNo;
+    this.EmailId=email;
+    this.Symptom=symptom;
+    this.Disease=disease;
+}
+
+function getVal(){
+    //getting details
+    var id=document.getElementById("pId").value;
+    var name=document.getElementById("pFname").value+document.getElementById("pMname").value+document.getElementById("pLname").value;
+    var age=document.getElementById("pAge").value;
+    var gender=document.getElementById("pGndr").value;
+    var height=document.getElementById("pHt").value;
+    var weight=document.getElementById("pWt").value;
+    var MoNo=document.getElementById("pMoNo").value;
+    var email=document.getElementById("pEmail").value;
+    var symptom=document.getElementById("symptom").value;
+    var disease=document.getElementById("symptomD").value; 
+    var sNo = new patientInfo(id,name,age,gender,height,weight,MoNo,email,symptom,disease);
+    
+}
+
+
+
+
+
+
+
+
  
